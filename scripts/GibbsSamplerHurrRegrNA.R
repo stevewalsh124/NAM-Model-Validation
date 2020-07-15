4b64f3e2bc0f2ba8b2980287126aa145ef82f0cd
Steve Walsh <walsh124@vt.edu>
2020-07-15 09:59:59 -0400

    add png creation for MLE/post plot for paper

--- scripts/GibbsSamplerHurrRegrNA.R
+++ scripts/GibbsSamplerHurrRegrNA.R
@@ -455,3 +455,52 @@ dev.off()
 rnorm(5)
 
 save.image(file = "~/NAM-Model-Validation/RData/Gibbs_flatPW.RData")
+
+
+
+## Plot MLE vs posterior estimates for the 47 storms
+png("~/NAM-Model-Validation/png/NMV_compareMLEpost.png", width = 1400, height = 700)
+par(mar=c(4.5,1.5,1.5,1.5), mfrow=c(1,3))
+for (i in 1:P) {
+  plot(0, 0, col = "white", ylab = "", 
+       xlim=c(min(theta_hat[,i], emp_thetaMED[,i])-0.1,
+              max(theta_hat[,i], emp_thetaMED[,i])+0.1),
+       ylim=c(0,2), yaxt='n', cex = 3, cex.axis =2, cex.lab = 2,
+       xlab = bquote(theta[.(i)]~"bottom and"~hat(theta)[.(i)]~"top"))
+  # mult_seg <- data.frame(x0 = c(0.7, 0.2, - 0.9, 0.5, - 0.2),    # Create data frame with line-values
+  #                        y0 = c(0.5, 0.5, 0.6, - 0.3, 0.4),
+  #                        x1 = c(0, 0.2, 0.4, - 0.3, - 0.6),
+  #                        y1 = c(- 0.1, 0.3, - 0.6, - 0.8, 0.9))
+  
+  segments(x0 = emp_thetaMED[,i],                            # Draw multiple lines
+           y0 = 0,
+           x1 = theta_hat[,i],
+           y1 = 1, col = loc_int$loc, lwd=3)
+  points(x=emp_thetaMED[,i], y=rep(0, length(emp_thetaMED[,i])), col= loc_int$loc)
+  points(x=theta_hat[,i],    y=rep(1, length(emp_thetaMED[,i])), col= loc_int$loc)
+  for (j in 1:N) {
+    #top row, theta hats
+    xseq <- seq(theta_hat[j,i]-3*sqrt(solve(hessians[[j]])[i,i]),
+                theta_hat[j,i]+3*sqrt(solve(hessians[[j]])[i,i]),
+                length.out = 1000)
+    lines(xseq,smush[i]*dnorm(xseq,
+                              theta_hat[j,i],
+                              sqrt(solve(hessians[[j]])[i,i]))+1, col=loc_int$loc[j],
+          lty = as.integer(loc_int$loc)[j], lwd=3)
+    
+    #bottom row, thetas from MCMC
+    xseq <- seq(min(theta_burn[[j]][,i]),
+                max(theta_burn[[j]][,i]), 
+                length.out = 1000)
+    lines(xseq, smush[i]*dnorm(xseq, 
+                               mean(theta_burn[[j]][,i]), 
+                               sd(theta_burn[[j]][,i])), col=loc_int$loc[j],
+          lty = as.integer(loc_int$loc)[j], lwd=3)
+  }
+  legend(max(theta_hat[,i], emp_thetaMED[,i])-
+           (max(theta_hat[,i], emp_thetaMED[,i])-min(theta_hat[,i], emp_thetaMED[,i]))*.4, 
+         2, lwd=3,
+         legend=c(levels(loc_int$loc)[unique(as.integer(loc_int$loc))]),
+         col=unique(as.integer(loc_int$loc)), lty=unique(as.integer(loc_int$loc)), cex=2)
+}
+dev.off()
