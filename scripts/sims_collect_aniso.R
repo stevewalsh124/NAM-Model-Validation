e428e7a4344750b5f3ddcf495d7b54808d1aa0d7
Steve Walsh <walsh124@vt.edu>
2021-03-09 21:47:42 -0500

    minor chgs to pdf paths, etc

--- scripts/sims_collect_aniso.R
+++ scripts/sims_collect_aniso.R
@@ -1,26 +1,29 @@
 # Collect anisotropic sim results
 
-set <- 4
-truths <- read.csv(paste0("~/NAM-Model-Validation/csv/aniso/collect/set",set,"/set",set,"truths.csv"))
+set <- 9
+truths <- read.csv(paste0("~/NAM-Model-Validation/csv/aniso/set",set,"/set",set,"truths.csv"))
 box <- 20
 
 truestart <- T
 
 # csvs for aniso sims on [0,box]^2 
 if(truestart){
-  csvs <- list.files(paste0("~/NAM-Model-Validation/csv/aniso/set",set), pattern = "truestart", full.names = T)
+  csvs <- list.files(paste0("~/NAM-Model-Validation/csv/aniso/set",set), 
+                     pattern = "truestart", full.names = T)
 } else {
-  csvs <- list.files(paste0("~/NAM-Model-Validation/csv/aniso/set",set), pattern = paste0("box",box), full.names = T)
+  csvs <- list.files(paste0("~/NAM-Model-Validation/csv/aniso/set",set), 
+                     pattern = paste0("box",box), full.names = T)
   csvs <- grep(csvs, pattern='truestart', invert=TRUE, value=TRUE)
 }
 
 tables <- lapply(csvs, read.csv, header = TRUE)
 MLEs <- do.call(rbind , tables)[,-1]
-pdf(paste0("~/NAM-Model-Validation/pdf/aniso/aniso_sim_results_box", box,"_set",set,if(truestart){"truestart"},".pdf"))
+pdf(paste0("~/NAM-Model-Validation/pdf/aniso/collect/aniso_sim_results_box", 
+           box,"_set",set,if(truestart){"truestart"},".pdf"))
 
 par(mfrow=c(3,2))
 for (i in 1:6) {
-  hist(MLEs[,i],  main = paste(truths[i,1],": all results"))
+  hist(MLEs[,i],  main = paste(truths[i,1],": ", round(truths[i,2],3)))
   abline(v=truths[i,2], lwd=2, col="blue")
 }
 
