f8d33417e2b87b7f1880ca400815115e5b6f00cd
Steve Walsh <walsh124@vt.edu>
2021-04-14 12:28:46 -0400

    include MLE vs Gibbs coverage in sims_eval_expntl; remove fixed truth eval, only eval when B and Sigma_theta are true generators

--- scripts/sims_eval_expntl.R
+++ scripts/sims_eval_expntl.R
@@ -1,15 +1,23 @@
+# look at sim coverages for MLEs vs Gibbs
+
+MLE_files <- list.files("~/NAM-Model-Validation/csv/myMLEsimcovers/MLEcovers", full.names = T)
+Gibbs_files <- list.files("~/NAM-Model-Validation/csv/myMLEsimcovers/Gibbscoverages", full.names = T)
+
+MLEcoverages <- Gibbscoverages <- c()
+
+for (i in 1:100) {
+  MLEcoverages <- rbind(MLEcoverages, read.csv(MLE_files[i])[,-1])
+  Gibbscoverages <- rbind(Gibbscoverages, read.csv(Gibbs_files[i])$x)
+}
+
+colnames(MLEcoverages) <- c("trutheta1","trutheta2")
+colMeans(MLEcoverages, na.rm = T)
+
+colnames(Gibbscoverages) <- c("theta1hat","theta2hat","trutheta1","trutheta2","truBcomps","truSTcomps")
+colMeans(Gibbscoverages, na.rm = T)
+
+
 ## look at sim results to check approx 95% coverage for theta1 and theta2
-
-black <- rgb(0,0,0,1)
-orange <- rgb(230/255,159/255,0,1)
-skyblue <- rgb(86/255,180/255,233/255,1)
-bluegreen <- rgb(0,158/255,115/255,1)
-yellow <- rgb(240/255,228/255,66/255,1)
-blue <- rgb(0,114/255,178/255,1)
-verm <- rgb(213/255, 94/255, 0,1)
-redpurp <- rgb(204/255, 121/255, 167/255, 1)
-
-my_cols <- c(skyblue,yellow,verm)
 
 seeds <- 1:50
 # sims 1-50 had these values fixed, with no variation
@@ -32,24 +40,10 @@ if(all(seeds == 51:100)){
 }
 
 # note the [seeds] at the end of these
-cover_files <- list.files("~/NAM-Model-Validation/csv/myMLEsimcovers", pattern = "seed", full.names = T)#[seeds]
+cover_files <- list.files("~/NAM-Model-Validation/csv/myMLEsimcovers/MLEcovers", pattern = "seed", full.names = T)#[seeds]
 theta_files <- list.files("~/NAM-Model-Validation/csv/myMLEsimcovers/thetas", full.names = T)#[seeds]
 sd_files <- list.files("~/NAM-Model-Validation/csv/myMLEsimcovers/thetas_sds", full.names = T)#[seeds]
 
-
-# the following are quick fixes since 
-# seed100 is not at the end of the list
-if(all(seeds == 1:50)){
-  cover_files <- cover_files[-c(11,52:100)]
-  theta_files <- theta_files[-c(11,52:100)]
-  sd_files <- sd_files[-c(11,52:100)]
-} 
-
-if(all(seeds == 51:100)){
-  cover_files <- cover_files[c(11,52:100)]
-  theta_files <- theta_files[c(11,52:100)]
-  sd_files <- sd_files[c(11,52:100)]
-} 
 
 
 # number of theta estimates, sd estimates, and coverage files should be the same
@@ -63,9 +57,9 @@ for (i in 1:m) {
   sd_means[i,] <- colMeans(read.csv(sd_files[i]))
 }
 
-colMeans(sim_means)
-colMeans(theta_means)
-colMeans(sd_means)
+colMeans(sim_means, na.rm = T)
+colMeans(theta_means, na.rm = T)
+colMeans(sd_means, na.rm = T)
 
 
 
@@ -84,6 +78,12 @@ sigma2hats <- exp(theta2hats)
 
 th_cor <- round(cor(theta1hats, theta2hats), 3)
 orig_cor <- round(cor(phihats, sigma2hats), 3)
+
+# Colors ideal for color-blind; full palette in NMV_plotting.R
+skyblue <- rgb(86/255,180/255,233/255,1)
+yellow <- rgb(240/255,228/255,66/255,1)
+verm <- rgb(213/255, 94/255, 0,1)
+my_cols <- c(skyblue,yellow,verm)
 
 png(paste0("~/NAM-Model-Validation/png/myMLEsim_results_seeds",
            min(seeds),"-",max(seeds),".png"), width = 3000, height = 2000, res=350)
@@ -174,5 +174,5 @@ plot(pixels, rowMeans(theta1_evals))
 plot(pixels, rowMeans(theta2_evals))
 
 cor(pixels, rowMeans(theta1_evals))	
-cor(pixels, rowMeans(theta2_evals))
+cor(pixels, rowMeans(theta2_evals, na.rm = T))
 
