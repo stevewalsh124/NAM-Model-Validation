ca92ab30c08585ebbc6bb9a5e86f7646f38722aa
Steve Walsh <walsh124@vt.edu>
2021-04-28 14:00:46 -0400

    add all 3 models into FL basin regime

--- scripts/FL_basin.R
+++ scripts/FL_basin.R
@@ -12,58 +12,78 @@ require(rgdal)
 require(sf)
 require(raster)
 library(fields)
-shape <- readOGR(dsn = "Florida_Drainage_Basins_1997", layer = "Florida_Drainage_Basins_1997")
-# plot(shape)
-apala = subset(shape, HUC %in% c("03120001","03120002","03120003","03120004"))
 
-ext <- extent(apala)
-r <- raster(ext, res=.01)  
-r <- rasterize(apala, r, field=1)
-plot(1:5,ylim=c(27,32), xlim=c(-87,-82),type="n", asp=1)
-plot(r,add=T)
-US(add=T)
+make_shp2raster <- F
 
-# writeRaster(r, "FLmask")
+if(make_shp2raster){
+  shape <- readOGR(dsn = "Florida_Drainage_Basins_1997", layer = "Florida_Drainage_Basins_1997")
+  # plot(shape)
+  apala = subset(shape, HUC %in% c("03120001","03120002","03120003","03120004"))
+  ext <- extent(apala)
+  r <- raster(ext, res=.01)  
+  r <- rasterize(apala, r, field=1)
+  plot(1:5,ylim=c(27,32), xlim=c(-87,-82),type="n", asp=1)
+  plot(r,add=T)
+  US(add=T)
+  writeRaster(r, "FLmask")
+}
 
-# shape2 <- read_sf(dsn = ".", layer = "SHAPEFILE")
-# shape2 <- st_read("Florida_Drainage_Basins_1997/Florida_Drainage_Basins_1997.shp")
-# gch <- gConvexHull(methods::as( object = apala, Class = "Spatial" ))
-# gb <- gBuffer(methods::as( object = apala, Class = "Spatial" )) 
-# rss <- readShapeSpatial("Florida_Drainage_Basins_1997/Florida_Drainage_Basins_1997.shp")
+
 pdf("pdf/FL_basin.pdf")
 FL_mask <- raster("FLmask.grd")
 plot(NA, xlim = c(-87,-82), ylim = c(27,32), type="n", asp=1)
 plot(FL_mask, add=T)
 US(add=T)
-mask_FL <- resample(FL_mask, rasterFromXYZ(cbind(coords, simvals[,1])))
+load(paste0("RData/prediction4"))
+mask_FL <- resample(FL_mask, rasterFromXYZ(cbind(coords, simvals[,1])), method="ngb")
 
 for (k in 1:6) {
-  print(k)
-  load(paste0("RData/prediction",k))
-  
-  sim1 <- rasterFromXYZ(cbind(coords, NAM_pred[,3]+simvals[,1])) * mask_FL
-  plot(sim1, main=paste0("storm ", k, ": ", name)); US(add=T)
-  sum(unique(values(sim1)),na.rm = T)
-  
-  sum_rains <- sum_sq_rains <- c()
-  for (i in 1:Ngen) {
-    sim1 <- rasterFromXYZ(cbind(coords, NAM_pred[,3]+simvals[,i])) * mask_FL
-    values(sim1)[which(values(sim1) < 0 )] <- 0
-    sum_rains[i] <- sum(values(sim1),na.rm = T)
-    sum_sq_rains[i] <- sum(values(sim1)^2,na.rm = T)
+  for (m in 1:3) {
+    print(paste0("storm ", k, ", model ", m))
+    if(m==1) load(paste0("RData/prediction",k))
+    if(m==2) load(paste0("RData/prediction",k,"_LM2"))
+    if(m==3) load(paste0("RData/prediction",k,"_LM3"))
+    
+    par(mfrow=c(1,1), mar = c(5,4,4,2)+0.1)
+    sim1 <- rasterFromXYZ(cbind(coords, NAM_pred[,3]+simvals[,1])) * mask_FL
+    if(m==1) plot(sim1, main=paste0("storm ", k, ": ", name)); US(add=T)
+    sum(unique(values(sim1)),na.rm = T)
+    
+    sum_rains <- sum_sq_rains <- c()
+    for (i in 1:Ngen) {
+      sim1 <- rasterFromXYZ(cbind(coords, NAM_pred[,3]+simvals[,i])) * mask_FL
+      values(sim1)[which(values(sim1) < 0 )] <- 0
+      sum_rains[i] <- sum(values(sim1),na.rm = T)
+      sum_sq_rains[i] <- sum(values(sim1)^2,na.rm = T)
+    }
+    
+    NAM_l <- sum(values(rasterFromXYZ(NAM_pred) * mask_FL),na.rm = T)
+    ST4_l <- sum(values(rasterFromXYZ(ST4_pred[,c(3,4,2)]) * mask_FL),na.rm = T)
+    NAM_l_sq <- sum(values(rasterFromXYZ(NAM_pred) * mask_FL)^2,na.rm = T)
+    ST4_l_sq <- sum(values(rasterFromXYZ(ST4_pred[,c(3,4,2)]) * mask_FL)^2,na.rm = T)
+    
+    par(mfrow=c(2,2), mar = c(2,2,2,1)+0.1)
+    hist(sum_rains, main=paste0("total sqrt(mm) precip,\n model ",m," storm ", k, ": ", name))
+    abline(v=NAM_l, col="green")
+    abline(v=ST4_l, col="blue")
+    
+    hist(sum_sq_rains, main=paste0("total mm precip,\n model ",m," storm ", k, ": ", name))
+    abline(v=NAM_l_sq, col="green")
+    abline(v=ST4_l_sq, col="blue")
+    
+    FL_pixs <- dim(rasterToPoints(sim1))[1]
+    if(FL_pixs > 0){
+      hist((sum_sq_rains*1e3*9834.98)/(FL_pixs), 
+      main=paste0("total m^3 in basin, precip,\n model ",m," storm ", k, ": ", name))
+      abline(v=(NAM_l_sq*1e3*9834.98)/(FL_pixs), col="green")
+      abline(v=(ST4_l_sq*1e3*9834.98)/(FL_pixs), col="blue")
+    }
   }
-  
-  NAM_l <- sum(values(rasterFromXYZ(NAM_pred) * mask_FL),na.rm = T)
-  ST4_l <- sum(values(rasterFromXYZ(ST4_pred[,c(3,4,2)]) * mask_FL),na.rm = T)
-  NAM_l_sq <- sum(values(rasterFromXYZ(NAM_pred) * mask_FL)^2,na.rm = T)
-  ST4_l_sq <- sum(values(rasterFromXYZ(ST4_pred[,c(3,4,2)]) * mask_FL)^2,na.rm = T)
-  
-  hist(sum_rains, main=paste0("total sqrt(mm) precip, storm ", k, ": ", name))
-  abline(v=NAM_l, col="green")
-  abline(v=ST4_l, col="blue")
-  
-  hist(sum_sq_rains, main=paste0("total mm precip, storm ", k, ": ", name))
-  abline(v=NAM_l_sq, col="green")
-  abline(v=ST4_l_sq, col="blue")
 }
+
 dev.off()
+
+mean_sq_rain <- 11472.2/101 # mean mm value for Michael 2018 (s=4) (sum of mm / # pixels)
+mean_km_rain <- mean_sq_rain/1e6 # .00011 km, about 4.33 inches in each pixel
+total_vol_km3 <- mean_km_rain * 9834.98 # 1.117117 cubic km
+total_vol_m3 <- total_vol_km3 * 1e9 # 1.12e9 cubic meters over the watershed
