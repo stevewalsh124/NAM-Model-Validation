4e2f859db52975b3ab0d803b7201ab37d4826ee5
Steve Walsh <walsh124@vt.edu>
2022-01-31 15:41:15 -0500

    scores by region andd overall

--- scripts/scores_by_basin.R
+++ scripts/scores_by_basin.R
@@ -1,9 +1,55 @@
-# evaluate basin predictive densities with log score
+# evaluate basin predictive densities with log score (overall)
 
-logS_files <- list.files("~/NAM-Model-Validation/csv/scores/basinwide", full.names = T, pattern = "storm[1-6].csv")
+logS_files <- list.files("~/NAM-Model-Validation/csv/scores/basinwide/", full.names = T, pattern = "storm[1-6].csv")
 CRPS_files <- list.files("~/NAM-Model-Validation/csv/scores/basinwide", full.names = T, pattern = "CRPS")
 logS_marf <- list.files("~/NAM-Model-Validation/csv/scores/basinwide/alt", full.names = T, pattern = ".csv")
 logS_sum_marf <- list.files("~/NAM-Model-Validation/csv/scores/basinwide/altsum", full.names = T, pattern = ".csv")
+
+logScores <- do.call(rbind, lapply(logS_files, read.csv, row.names = 1))
+logScores <- logScores[complete.cases(logScores),]
+CRPScores <- do.call(rbind, lapply(CRPS_files, read.csv, row.names = 1))
+CRPScores <- CRPScores[complete.cases(CRPScores),]
+logScores_M <- do.call(rbind, lapply(logS_marf, read.csv, row.names = 1))
+logScores_M <- logScores_M[complete.cases(logScores_M),]
+logScores_SM <- do.call(rbind, lapply(logS_sum_marf, read.csv, row.names = 1))
+logScores_SM <- logScores_SM[complete.cases(logScores_SM),]
+
+# find the mean wrt the original (NOT log) scale
+f <- function(X){log(mean(exp(X - max(X)))) + max(X)}
+f2 <- function(X){log(median(exp(X - max(X)))) + max(X)}
+
+# # mean of logScores
+# sort(-apply(logScores, 2, f)) #V5 is best
+# sort(apply(logScores_M, 2, f)) #V4 is best, V5 is 2nd best
+# sort(-apply(CRPScores, 2, f)) #V4 is best, V5 is 2nd best
+# # apply(logScores_SM, 2, f) #V4 is best, V5 is 2nd best
+
+# best logScore overall
+# sort(-colSums(logScores)) #V5 is best again
+sort(colSums(logScores_M)) #V5 is best
+# sort(-colSums(CRPScores))
+# colSums(logScores_SM) #V5 is best
+
+table(apply(logScores_M,1,which.max))
+# table(apply(logScores_M,1,which.min))
+
+# table(apply(CRPScores,1,which.max))
+# table(apply(CRPScores,1,which.min))
+
+# # median of logScores # not as useful 
+# apply(logScores, 2, median) #V6 is best; doesn't penalize its shorter tails as much
+# apply(logScores_M, 2, median) #V5 is best, V4 is 2nd best
+# apply(logScores, 2, median) #V5 is best, V4 is 2nd best
+
+
+
+
+# evaluate basin predictive densities with log score (by region)
+
+logS_files <- list.files("~/NAM-Model-Validation/csv/scores/basinwide/by_region/", full.names = T, pattern = "storm[1-6].csv")
+CRPS_files <- list.files("~/NAM-Model-Validation/csv/scores/basinwide/by_region/", full.names = T, pattern = "CRPS")
+logS_marf <- list.files("~/NAM-Model-Validation/csv/scores/basinwide/by_region/alt", full.names = T, pattern = ".csv")
+logS_sum_marf <- list.files("~/NAM-Model-Validation/csv/scores/basinwide/by_region/altsum", full.names = T, pattern = ".csv")
 
 logScores <- do.call(rbind, lapply(logS_files, read.csv, row.names = 1))
 logScores <- logScores[complete.cases(logScores),]
@@ -18,54 +64,35 @@ logScores_SM <- logScores_SM[complete.cases(logScores_SM),]
 f <- function(X){log(mean(exp(X - max(X)))) + max(X)}
 f2 <- function(X){log(median(exp(X - max(X)))) + max(X)}
 
-# mean of logScores
-apply(logScores, 2, f) #V5 is best
-apply(logScores_M, 2, f) #V4 is best, V5 is 2nd best
-apply(CRPScores, 2, f) #V4 is best, V5 is 2nd best
-# apply(logScores_SM, 2, f) #V4 is best, V5 is 2nd best
+# # mean of logScores
+# sort(-apply(logScores, 2, f)) #V5 is best
+# sort(apply(logScores_M, 2, f)) #V4 is best, V5 is 2nd best
+# sort(-apply(CRPScores, 2, f)) #V4 is best, V5 is 2nd best
+# # apply(logScores_SM, 2, f) #V4 is best, V5 is 2nd best
 
 # best logScore overall
-colSums(logScores) #V5 is best again
-colSums(logScores_M) #V5 is best
-colSums(CRPScores)
+# sort(-colSums(logScores)) #V5 is best again
+sort(colSums(logScores_M[logScores_M[,10]=="A",c(-10,-11)])) #V5 is best
+sort(colSums(logScores_M[logScores_M[,10]=="F",c(-10,-11)])) #V5 is best
+sort(colSums(logScores_M[logScores_M[,10]=="G",c(-10,-11)])) #V5 is best
+# sort(-colSums(CRPScores))
+# colSums(logScores_SM) #V5 is best
+table(apply(logScores_M,1,which.max))
+table(apply(logScores_M[logScores_M[,10]=="A",c(-10,-11)],1,which.max))
+table(apply(logScores_M[logScores_M[,10]=="F",c(-10,-11)],1,which.max))
+table(apply(logScores_M[logScores_M[,10]=="G",c(-10,-11)],1,which.max))
+table(apply(logScores_M[logScores_M[,11]=="N",c(-10,-11)],1,which.max))
+table(apply(logScores_M[logScores_M[,11]=="C",c(-10,-11)],1,which.max))
+# table(apply(logScores_M,1,which.min))
+
+# best logScore overall
+# sort(-colSums(logScores)) #V5 is best again
+sort(colSums(logScores_M[logScores_M[,11]=="N",c(-10,-11)])) #V5 is best
+sort(colSums(logScores_M[logScores_M[,11]=="C",c(-10,-11)])) #V5 is best
+# sort(-colSums(CRPScores))
 # colSums(logScores_SM) #V5 is best
 
-# # median of logScores # not as useful 
-# apply(logScores, 2, median) #V6 is best; doesn't penalize its shorter tails as much
-# apply(logScores_M, 2, median) #V5 is best, V4 is 2nd best
-# apply(logScores, 2, median) #V5 is best, V4 is 2nd best
+table(apply(logScores_M[logScores_M[,11]=="N",c(-10,-11)],1,which.max))
+table(apply(logScores_M[logScores_M[,11]=="C",c(-10,-11)],1,which.max))
+# table(apply(logScores_M,1,which.min))
 
-# THESE ARE NOT IDEAL SINCE MEAN, ETC SHOULD BE CALCULATED ON THE ORIGINAL,
-# NOT LOG, SCALE. SO, MAKE THIS ADJUSTMENT FIRST WITH F ABOVE
-# colMeans(logScores, na.rm = T)
-# apply(logScores, 2, median, na.rm=T)
-# 
-# the_best <- c()
-# for (i in 1:length(logS_files)) {
-#   logScore <- read.csv(logS_files[i], row.names = 1)
-#   the_best[i] <- which.min(colMeans(logScore, na.rm = T))
-# }
-# the_best
-# table(the_best)
-# 
-# the_best2 <- c()
-# for (i in 1:nrow(logScores)) {
-#   if(is.na(logScores[i,])) next
-#   the_best2[i] <- which.min(logScores[i,])
-# }
-# the_best2
-# table(the_best2)
-
-load("~/NAM-Model-Validation/RData/prediction/prediction1subtractpw.RData")
-dim(simvals)
-sum(simvals <= 0)
-plot(rasterFromXYZ(cbind(NAM_pred$x, NAM_pred$y, NAM_pred$value)))
-
-
-load("~/NAM-Model-Validation/RData/prediction/prediction1nopw")
-dim(simvals)
-sum(simvals <= 0)
-plot(rasterFromXYZ(cbind(NAM_pred$x, NAM_pred$y, NAM_pred$value)))
-
-meanz <- rowMeans(simvals)
-plot(rasterFromXYZ(cbind(NAM_pred$x, NAM_pred$y, meanz+NAM_pred$value)))
