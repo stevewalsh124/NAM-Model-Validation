e1cf8ac25cbec7d4bf975e1e66efbbf4fc8a5ae2
Steve Walsh <walsh124@vt.edu>
2022-06-16 14:36:28 -0400

    init commit rain intervals

---
+++ scripts/rain_intervals.R
@@ -0,0 +1,55 @@
+par(mfrow=c(2,2))
+UBs_int95s <- LBs_int95s <- avg_int95s <- max_int95s <- diffs_in <- c()
+npixs <- c()
+for (ste in 1:6) {
+  print(ste)
+  load(paste0("~/NAM-Model-Validation/RData/prediction/prediction",ste,"nopw.RData"))
+  
+  # 1000 rain values for each grid point
+  rain_scen <- ifelse(simvals+NAM_pred$value>0, simvals+NAM_pred$value, 0)^2
+  hist(rain_scen)
+  
+  # look at entire range for each grid point
+  rain_ints <- apply(rain_scen, 1, function(X){diff(range(X))})
+  hist(rain_ints, breaks=40)
+  
+  # look at 95% CI at each grid point
+  rain_ints95 <- apply(rain_scen, 1, function(X){quantile(X,0.975)-quantile(X,0.025)})
+  hist(rain_ints95, breaks=40)
+  
+  # Find the 95% upper bound for these 95% CIs
+  UBs_int95s[ste] <- quantile(rain_ints95,0.95)
+  # Find the average length of these 95% CIs
+  avg_int95s[ste] <- mean(rain_ints95)
+  # Find the 5% lower bound for these 95% CIs
+  LBs_int95s[ste] <- quantile(rain_ints95,0.05)
+  # Find the maximum CI
+  max_int95s[ste] <- max(rain_ints95)
+  # Look how forecasted value affects CIs
+  plot(NAM_pred$value^2, rain_ints95)
+  
+  # Gather number of grid points to weight the average
+  npixs[ste] <- length(NAM_pred$value)
+  
+  print(paste("NAM",max(NAM_pred$value)^2/25.4))
+  print(paste("ST4",max(ST4_pred$value)^2/25.4))
+  print(paste("abs diff",max(abs(ST4_pred$value^2-NAM_pred$value^2)/25.4)))
+  diffs_in <- c(diffs_in, abs(ST4_pred$value^2-NAM_pred$value^2)/25.4)
+}
+
+# Weighted average of 95% upper bound for length of 95% confidence intervals
+# Also, Weighted average of 95% confidence intervals
+# 25.4mm for every inch
+sum(npixs*UBs_int95s)/sum(npixs)/25.4 # 4.322 inches
+sum(npixs*avg_int95s)/sum(npixs)/25.4 # 1.605 inches
+max(max_int95s)/25.4 # 1.605 inches
+
+# Margin of error is half the length of CI
+sum(npixs*UBs_int95s)/sum(npixs)/25.4/2 # 4.322 inches
+sum(npixs*avg_int95s)/sum(npixs)/25.4/2 # 1.605 inches
+max(max_int95s)/25.4/2 # 1.605 inches
+
+# Look at 95%, 99% UBs and max absolute difference of NAM & ST4
+quantile(diffs_in, 0.95)
+quantile(diffs_in, 0.99)
+max(diffs_in)
