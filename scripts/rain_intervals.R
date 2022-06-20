98b36567ae1f2efb7bcf29ca04fc6c0b13b12327
Steve Walsh <walsh124@vt.edu>
2022-06-20 10:15:01 -0400

    add average absolute differences for different thresholds of the test storms

--- scripts/rain_intervals.R
+++ scripts/rain_intervals.R
@@ -3,7 +3,10 @@
 library(raster)
 
 par(mfrow=c(2,2))
-UBs_int95s <- LBs_int95s <- avg_int95s <- max_int95s <- diffs_in <- c()
+UBs_int95s <- LBs_int95s <- avg_int95s <- max_int95s <- 
+  diffs_in <- diffs_in_gr0 <- diffs_in_gr1 <- diffs_in_gp1i <- diffs_in_gp3i <- 
+  act_rain <- act_rain_gr0 <- act_rain_gr1 <- act_rain_gp1i <- 
+  act_rain_UB <- act_rain_UB_gp1i <- c()
 npixs <- c()
 for (ste in 1:6) {
   print(ste)
@@ -32,13 +35,27 @@ for (ste in 1:6) {
   # Look how forecasted value affects CIs
   plot(NAM_pred$value^2, rain_ints95)
   
+  # Gather the mean amount of rain for each storm in buffer region
+  act_rain[ste] <- mean(ST4_pred$value^2)/25.4
+  act_rain_gr0[ste] <- mean(ST4_pred$value[ST4_pred$value>0]^2)/25.4
+  act_rain_gr1[ste] <- mean(ST4_pred$value[ST4_pred$value>1]^2)/25.4
+  act_rain_gp1i[ste] <- mean(ST4_pred$value[ST4_pred$value>2.54]^2)/25.4
+  act_rain_UB[ste] <-  quantile(ST4_pred$value^2, 0.95)/25.4
+  act_rain_UB_gp1i[ste] <-  quantile(ST4_pred$value[ST4_pred$value>2.54]^2, 0.95)/25.4
+  
+  # Gather the differences between obs and forecast
+  
   # Gather number of grid points to weight the average
   npixs[ste] <- length(NAM_pred$value)
   
-  print(paste("NAM",max(NAM_pred$value)^2/25.4))
-  print(paste("ST4",max(ST4_pred$value)^2/25.4))
-  print(paste("abs diff",max(abs(ST4_pred$value^2-NAM_pred$value^2)/25.4)))
+  print(paste("max NAM (in):",max(NAM_pred$value)^2/25.4))
+  print(paste("max ST4 (in):",max(ST4_pred$value)^2/25.4))
+  print(paste("max abs diff (in):",max(abs(ST4_pred$value^2-NAM_pred$value^2)/25.4)))
   diffs_in <- c(diffs_in, abs(ST4_pred$value^2-NAM_pred$value^2)/25.4)
+  diffs_in_gr0 <- c(diffs_in_gr0, abs(ST4_pred$value[ST4_pred$value>0]^2-NAM_pred$value[ST4_pred$value>0]^2)/25.4)
+  diffs_in_gr1 <- c(diffs_in_gr1, abs(ST4_pred$value[ST4_pred$value>1]^2-NAM_pred$value[ST4_pred$value>1]^2)/25.4)
+  diffs_in_gp1i <- c(diffs_in_gp1i, abs(ST4_pred$value[ST4_pred$value>2.54]^2-NAM_pred$value[ST4_pred$value>2.54]^2)/25.4)
+  diffs_in_gp3i <- c(diffs_in_gp3i, abs(ST4_pred$value[ST4_pred$value>2.54*3]^2-NAM_pred$value[ST4_pred$value>2.54*3]^2)/25.4)
 }
 
 # Weighted average of 95% upper bound for length of 95% confidence intervals
@@ -57,3 +74,20 @@ max(max_int95s)/25.4/2 # 1.605 inches
 quantile(diffs_in, 0.95)
 quantile(diffs_in, 0.99)
 max(diffs_in)
+
+# Look at actual rain amount under different thresholds
+act_rain
+act_rain_gr0
+act_rain_gr1
+act_rain_gp1i
+
+act_rain_UB
+act_rain_UB_gp1i
+
+# Average absolute difference b/w observed and forecasted precip,
+# with different thresholds for observed precip (>0, >1mm, >.1in, >.3in)
+mean(diffs_in)
+mean(diffs_in_gr0)
+mean(diffs_in_gr1)
+mean(diffs_in_gp1i)
+mean(diffs_in_gp3i)
